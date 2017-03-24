2a9134a12e09fc1f736a5afd4e6d6922f0c875ca
nmcadamsmadura <adamsb2@duq.edu>
2017-03-24 12:37:08 -0400

    Added comments and explanations. Removed extra code.

--- OT.m
+++ OT.m
@@ -1,30 +1,34 @@
-function [] = OT()   
+%Written by Brian Adams
+%Version
+function [] = OT()
 format long g
 uiwait(msgbox('Welcome to the program! Please select a .csv file with one excercise!','modal'));
+
+%Here we get the file from user input
 filename = uigetfile;
 t = importdata(filename);
 
-
+%Then after getting the file we seperate the gyroData from the Accelerometer data.
+%We put them into the gyroData and accelData variables
 gy = t.data(:,1) == 4;
 gyroData = t.data(gy,1:4);
 gyroData = abs(gyroData);
-
-
 ac = t.data(:,1) == 3;
 accelData = t.data(ac,1:4);
 accelData = abs(accelData);
 
-length(gyroData)
-length(accelData)
+%We then take the sizes of both gyroData and accelData and determine which has the smallest size.
+%NOTE: This is due to the fact that the amount of time steps for accelData and gyroData can vary
+%thus to prevent any hiccups we just pick the smallest. Error is minimal considering most
+%workouts end within the the smallest size.
+size = min([length(gyroData);length(accelData)]);
 
-
-size = min([length(gyroData);length(accelData)])
+%This line collects all the time stamps so we can analyze them later on.
 time = t.textdata(1:size);
 
-fullsize = length (t.data(:,1));
-accelLineStart = length(gyroData)+1;
-
+%This line is to increase speed by preallocating this stuff in memory
 tmp = zeros([2,3]);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 prompt = {'Enter Patient Name','How many sets did they do of this excercise?'};
 dlg_title = 'Input';
@@ -32,6 +36,8 @@ num_lines = 1;
 defaultans = {'<enter name>','<enter a number>'};
 answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Graphs%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure('Name','Angular Velocity X','units','normalized','position',[0 .5 .3 .3]);
 plot(1:size,gyroData(1:size,2));
 xlabel('Line #') % x-axis label
@@ -79,11 +85,9 @@ ylabel('Position') % y-axis label
 grid on;
 datacursormode on
 set(gca, 'XTickLabel', num2str(get(gca, 'XTick')'));
-
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 answer2 = [];
 options.WindowStyle = 'normal';
 for i = 1:str2num(cell2mat(answer(2)))
@@ -96,45 +100,21 @@ end
 B = reshape(answer2,str2num(cell2mat(answer(2))),2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-%{
-ind = 1;
-while ind ~= size
-    tmp(1,1) = gyroData(ind,2);
-    tmp(1,2) = gyroData(ind,3);
-    tmp(1,3) = gyroData(ind,4);
-    tmp(2,1) = gyroData(ind+1,2);
-    tmp(2,2) = gyroData(ind+1,3);
-    tmp(2,3) = gyroData(ind+1,4);
-    DistanceAverage(ind) = ((tmp(2,1)-tmp(1,1))^2 + (tmp(2,2)-tmp(1,2))^2 + (tmp(2,3)-tmp(1,3))^2)^(1/2);
-    ind = ind + 1;
-end
-averageDistancesPerSet = [];
-cellAverage = [];
-for i = 1:length(answer2)/2
- averageDistancesPerSet = DistanceAverage(str2num(B{i,1}):str2num(B{i,2})-1);
- cellAverage = [cellAverage;sum(averageDistancesPerSet)];
-end 
-averageDistance = cellAverage;
-msg = cell(length(averageDistance),1);
-%}
-
-formatspec = ['Set %d\n\n', ... 
+formatspec = ['Set %d\n\n', ...
                     'Duration: ~ %d sec \n', ...
-                    'Average angular velocity(x): %f deg/s \n',... 
+                    'Average angular velocity(x): %f deg/s \n',...
                     'Average angular velocity(y): %f  deg/s \n',...
                     'Average angular velocity(z): %f  deg/s \n',...
                     'Total angular Velocity: %f deg/s \n',...
-                    'Average acceleration(x): %f meters/s^2 \n',... 
+                    'Average acceleration(x): %f meters/s^2 \n',...
                     'Average acceleration(y): %f meters/s^2 \n',...
                     'Average acceleration(z): %f meters/s^2 \n',...
                     'Total acceleration: %f \n'];
-                
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Now we need to find the total duration of the time stamps
 %To do this we need average time stops per minute.
-
 initTime = char(time(str2num(B{1,1})));
 timeWithoutChange = 0;
 actualSeconds = [];
@@ -156,7 +136,9 @@ for i = 1:length(answer2)/2
     avgTS = [avgTS; round(mean(stops))];
 end
 averageTimeStopsPerSecond = avgTS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 averageANG = [];
 totalANG = [];
 totalANG2 = [];
@@ -171,7 +153,7 @@ for i = 1:length(answer2)/2
         totalANG = [totalANG; total];
         averageANG = [averageANG; x,y,z];
         temp3 = temp3 + str2num(B{i,1});
-        
+
     end
     totalANG2 = [totalANG2;mean(totalANG)];
 end
@@ -186,17 +168,19 @@ for i = 1:length(answer2)/2
         y = mean(accelData(temp4:str2num(B{i,1}) + avgTS(k),3));
         z = mean(accelData(temp4:str2num(B{i,1}) + avgTS(k),4));
         total = x + y + z;
-        totalACC = [totalACC; total]; 
+        totalACC = [totalACC; total];
         averageACC = [averageACC; x,y,z];
         temp3 = temp3 + str2num(B{i,1});
     end
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Ths just prints out the analyzed averages data determined in the previous section.
 for i = 1:length(answer2)/2
    msg{i} = sprintf(formatspec,i,actualSeconds(i),averageANG(i,1),averageANG(i,2),averageANG(i,3),mean(totalANG2),averageACC(i,1),averageACC(i,2),averageACC(i,3),totalACC(i));
 end
 msgbox(msg);
 
-    
-    
+
+
 end
